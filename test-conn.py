e02ae06c9cf5c57bb2bdfe2fce8a2f20fa9e1b68
APresekal <presekal@ui.ac.id>
2021-02-07 06:22:40 -0500

    Add files via upload

--- test-conn.py
+++ test-conn.py
@@ -7,23 +7,12 @@ import datetime
 HOST2 = '131.180.165.26'
 PORT1 = 999
 
-# Define a function for the thread
-def serverOne():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s1:
-        s1.connect((HOST2, PORT1))
-        x = 1
-        while x < 6:
-            data1 = s1.recv(1024)
-            data1new = data1.decode("utf-8")
-            print(data1new)
 
-# Create two threads as follows
-try:
-   _thread.start_new_thread( serverOne, ( ) )
-   #_thread.start_new_thread( serverTwo, ( ) )
-except:
-   print ("Error: unable to start thread")
-
-while 1:
-   pass
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s1:
+    s1.connect((HOST2, PORT1))
+    x = 1
+    while x < 6:
+        data1 = s1.recv(1024)
+        data1new = data1.decode("utf-8")
+        print(data1new)
 
