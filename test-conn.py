1141d18b07c0434d008bce7b61fc21cbd456a918
APresekal <presekal@ui.ac.id>
2021-02-07 06:16:15 -0500

    Add files via upload

---
+++ test-conn.py
@@ -0,0 +1,21 @@
+import binascii
+import _thread
+import time
+import socket
+import datetime
+
+HOST2 = '131.180.165.26'
+PORT1 = 999
+
+# Define a function for the thread
+def serverOne():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s1:
+        s1.connect((HOST2, PORT1))
+        x = 1
+        while x < 6:
+            data1 = s1.recv(1024)
+            data1new = data1.decode("utf-8")
+            print(data1new)
+
+
+
